ecf4714951221977f96200f16ca31c3b4710bdb3
Hsiaoming Yang <lepture@me.com>
2013-01-03 16:26:40 +0800

    style file available

---
+++ tests/style.test.js
@@ -0,0 +1,20 @@
+var expect = require('expect.js');
+var style = require('../lib/style');
+
+
+describe('styleFile', function() {
+  it('style async', function(done) {
+    style.styleFile(__dirname + '/cases/one-section.styl', function(data) {
+      expect(data.css).to.contain('#d64;');
+      done();
+    });
+  });
+});
+
+
+describe('styleFileSync', function() {
+  it('style sync', function() {
+    var data = style.styleFileSync(__dirname + '/cases/one-section.styl');
+    expect(data.css).to.contain('#d64;');
+  });
+});
