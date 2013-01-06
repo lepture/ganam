9eb4b211873dc8959041e4eb46f692947facd911
Hsiaoming Yang <lepture@me.com>
2013-01-06 22:12:53 +0800

    delete writer, move into nico

--- tests/style.test.js
+++ tests/style.test.js
@@ -6,6 +6,7 @@ describe('styleFile', function() {
   it('style async', function(done) {
     style.styleFile(__dirname + '/cases/one-section.styl', function(data) {
       expect(data.css).to.contain('#d64;');
+      expect(data.order).to.equal(1);
       done();
     });
   });
@@ -16,5 +17,6 @@ describe('styleFileSync', function() {
   it('style sync', function() {
     var data = style.styleFileSync(__dirname + '/cases/one-section.styl');
     expect(data.css).to.contain('#d64;');
+    expect(data.order).to.equal(1);
   });
 });
