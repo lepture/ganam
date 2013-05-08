83ba9f2c112a52ca1f4a2cb12f147f68432ce06b
Hsiaoming Yang <me@lepture.com>
2013-05-08 17:50:46 +0800

    export version

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports = module.exports = require('./lib/parser');
 var style = require('./lib/style');
 exports.style = style.styleFile;
 exports.styleSync = style.styleFileSync;
+exports.version = require('./package').version;
